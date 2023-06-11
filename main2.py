b5dcbcced3a2a3e79fe829452dec585b9a6ad26b
Pablo Fernandes <pablo.fernandes11041992@gmail.com>
2023-06-11 16:00:45 -0300

    Projeto Pedra Papel e tesoura

---
+++ main2.py
@@ -0,0 +1,83 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+
+humano = input("Tente ganhar do computador! \nVocê deve escolher entre pedra, papel ou tesoura: ")
+if humano == "pedra":
+    print(rock)
+    humano = 0
+elif humano == "papel":
+    print(paper)
+    humano = 1
+else:
+    print(scissors)
+    humano = 2
+
+computador = random.randint(0,2)
+if computador == 0:
+    print(f"Computador escolheu \n {rock}")
+    computador = 0
+elif humano == 1:
+    print(f"Computador escolheu \n {paper}")
+    computador = 1
+else:
+    print(f"Computador escolheu \n {scissors}")
+    computador = 2
+
+vitoria = "Parabéns você ganhou"
+perdeu = "Mas que pena, você perdeu."
+empate = "Deu empate! Ninguem ganhou"
+
+if humano == 0:
+    if computador == 1:
+        print(perdeu)
+    elif computador == 2:
+        print(vitoria)
+    else:
+        print(empate)
+        
+elif humano == 1:
+    if computador == 1:
+        print(empate)
+    elif computador == 2:
+        print(perdeu)
+    else:
+        print(vitoria)
+
+elif humano == 2:
+    if computador == 1:
+        print(vitoria)
+    elif computador == 2:
+        print(empate)
+    else:
+        print(perdeu)
+
+else:
+    print("Erro de digitação, digite novamente.")
