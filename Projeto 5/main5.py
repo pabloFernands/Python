dda24081965db4ba84f7e54fe50cc500782fc5d9
Pablo Fernandes <pablo.fernandes11041992@gmail.com>
2023-06-27 20:13:38 -0300

    Projeto 5 criado

---
+++ Projeto 5/main5.py
@@ -0,0 +1,30 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from art import logo
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+        position = alphabet.index(char)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    else:
+       end_text += char
+  print(f"Aqui está o seu {cipher_direction}: {end_text}")
+
+print(logo)
+def start():
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    shift = shift % 26
+    print(shift)
+
+    repetir = input('Deseja repetir o programa? "Sim" ou "Não" \n')
+    if repetir == "sim":
+       start()
+
+start()
