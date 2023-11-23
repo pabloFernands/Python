8ef335560eec54f235b59d16613a7353d87c3696
Pablo Fernandes <pablo.fernandes11041992@gmail.com>
2023-11-23 19:38:56 -0300

    Projeto 20 em construção

---
+++ Projeto 20/main.py
@@ -0,0 +1,11 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("Map of USA")
+image_background = "blank_states_img.gif"
+screen.addshape(image_background)
+
+
+
+
+screen.exitonclick()
---
+++ Projeto 20/squirrel_teste/squirrel.py
@@ -0,0 +1,26 @@
+import csv
+import pandas
+
+#print(tempo_list)
+
+# tempo_maximo = data["temp"].max()
+# monday_day = data[data.day == "Monday"]
+# monday_temp = monday_day["temp"]
+#data_dict = data.to_dict()
+#print(data)
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+gray_count = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_count = len(data[data["Primary Fur Color"] == "Black"])
+#print(gray_count, cinnamon_count, black_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_count, cinnamon_count, black_count]
+}
+
+new_data = pandas.DataFrame(data_dict)
+print(new_data)
+new_data.to_csv("squirrel_count.csv")
